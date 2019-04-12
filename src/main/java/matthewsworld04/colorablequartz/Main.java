c1f16cd24f9b47dd94ca4397ca89710c641b80af
Matthewsworld <matthewsworldofficial@gmail.com>
2019-04-12 11:10:00 +0100

    Added dumb comment for testing

--- src/main/java/matthewsworld04/colorablequartz/Main.java
+++ src/main/java/matthewsworld04/colorablequartz/Main.java
@@ -17,6 +17,7 @@ public class Main {
 	@Instance
 	public static Main instance;
 	
+	//poxy proxy
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
 	public static CommonProxy proxy;
 	
